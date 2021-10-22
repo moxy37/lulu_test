761f0ebd876591a517e08ef0a5e8adc304cd6e86
moxy37 <dbrennan@nu-worx.com>
2021-10-22 04:49:37 -0600

    sdf

--- prod.py
+++ prod.py
@@ -14,7 +14,7 @@ with open('itemdata.csv') as csvDataFile:
     for o in csvReader:
         try:
             cursor = cnxn.cursor()
-            cursor.execute("INSERT INTO TestProducts (sku, deptCode, deptName, subDeptCode, subDeptName, classCode, className, subClassCode, subClassName, styleCode, styleName, price, store, total) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", o[0], o[2], o[6], o[3], o[8], o[4], o[10], o[5], o[12], o[1], o[14], float(o[15]), o[16], int(o[17])) 
+            cursor.execute("INSERT INTO TestProducts (sku, deptCode, deptName, subDeptCode, subDeptName, classCode, className, subClassCode, subClassName, styleCode, styleName, price, store, total) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (o[0], o[2], o[6], o[3], o[8], o[4], o[10], o[5], o[12], o[1], o[14], float(o[15]), o[16], int(o[17])) )
             cnxn.commit()
             added = added + 1
         except Exception as e:
