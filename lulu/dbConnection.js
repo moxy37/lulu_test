08c9a0a399f61feb2fbc2a65eedbf18b343824b1
moxy37 <dbrennan@nu-worx.com>
2021-10-22 15:52:49 -0600

    Trying more

--- lulu/dbConnection.js
+++ lulu/dbConnection.js
@@ -1,39 +1,11 @@
-// var mysql = require('mysql2');
+var mysql = require('mysql2');
 
-// var dbConnection = mysql.createPool({
-//     host:"localhost",
-//     connectionLimit: 10,
-//     user: "root",
-//     password: "root",
-//     database: "lulu"
-// });
+var dbConnection = mysql.createPool({
+    host:"localhost",
+    connectionLimit: 10,
+    user: "luluuser",
+    password: "Moxy..37",
+    database: "lulu"
+});
 
-// module.exports = dbConnection;
-
-var mysql = require('mysql');
-var settings = {
-    server: "localhost",
-    database: "Lulu",
-    driver: "msnodesqlv8",
-    options: {
-      trustedConnection: true
-    }
-  };
-var db;
-
-function connectDatabase() {
-    if (!db) {
-        db = mysql.createConnection(settings);
-
-        db.connect(function(err){
-            if(!err) {
-                console.log('Database is connected!');
-            } else {
-                console.log('Error connecting database!');
-            }
-        });
-    }
-    return db;
-}
-
-module.exports = connectDatabase();`
+module.exports = dbConnection;
