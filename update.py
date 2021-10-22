96e61dd4458175880e3f17fa8128f55133d803a3
moxy37 <dbrennan@nu-worx.com>
2021-10-22 15:18:39 -0600

    Just making this every minute instead

--- update.py
+++ update.py
@@ -15,8 +15,8 @@ for i in range(0, 10000):
     added = 0
     deleted = 0
     print("New query")
-    url = 'http://44.192.77.149/v1/bar/' + siteId + '/data/event/item?q=timestamp%3Agt(' + requests.utils.quote(str(now.replace(tzinfo=timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ'))) + ')&order=timestamp%3Adesc&size=200000&returnCount=false'
-    sleep(10)
+    url = 'http://44.192.77.149/v1/bar/' + siteId + '/data/event/item?q=timestamp%3Age(' + requests.utils.quote(str(now.replace(tzinfo=timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ'))) + ')&order=timestamp%3Adesc&size=200000&returnCount=false'
+    sleep(60)
     #print(str(url))
     headers = {'Authorization': '2993A070-1E86-4967-8C93-D592602EDD30', 'Accept': 'application/json' , 'Content-Type': 'application/json'}
 
