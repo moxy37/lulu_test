8afa2dd985ba0e03ac45e3ec0552d32d29d37695
moxy37 <dbrennan@nu-worx.com>
2021-10-25 06:52:20 -0600

    Removing old code

--- lulu/data/views.sql
+++
@@ -1,7 +0,0 @@
-
-
-CREATE VIEW AllDept AS SELECT deptCode, deptName, COUNT(*) AS total FROM Products GROUP BY deptCode, deptName;
-CREATE VIEW AllSubDept AS SELECT deptCode, deptName, subDeptCode, subDeptName, COUNT(*) AS total FROM Products GROUP BY deptCode, deptName, subDeptCode, subDeptName;
-CREATE VIEW AllClass AS SELECT deptCode, deptName, subDeptCode, subDeptName, classCode, className, COUNT(*) AS total FROM Products GROUP BY deptCode, deptName, subDeptCode, subDeptName, classCode, className;
-CREATE VIEW AllSubClass AS SELECT deptCode, deptName, subDeptCode, subDeptName, classCode, className, subClassCode, subClassName, COUNT(*) AS total FROM Products GROUP BY deptCode, deptName, subDeptCode, subDeptName, classCode, className, subClassCode, subClassName;
-CREATE VIEW AllStyle AS SELECT deptCode, deptName, subDeptCode, subDeptName, classCode, className, subClassCode, subClassName, styleCode, styleName, COUNT(*) AS total FROM Products GROUP BY deptCode, deptName, subDeptCode, subDeptName, classCode, className, subClassCode, subClassName, styleCode, styleName;
