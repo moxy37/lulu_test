544c1517c67c9b7cd3e8510879bea0a7cf4a15af
moxy37 <dbrennan@nu-worx.com>
2021-10-26 03:25:38 -0600

    Fixing sales

--- trans.py
+++ trans.py
@@ -8,6 +8,7 @@ import datetime
 cnxn = mysql.connector.connect(host="localhost", user="luluuser", passwd="Moxy..37Moxy..37", database="lulu")
 added = 0
 deleted = 0
+error = 0
 firstLine = True
 with open('sales.csv') as csvDataFile:
     csvReader = csv.reader(csvDataFile)
@@ -20,11 +21,16 @@ with open('sales.csv') as csvDataFile:
                 storeId = 'd4f87b6f-5199-43ac-b231-fbe6e3a8039c'
             try:
                 cursor = cnxn.cursor()
-                cursor.execute("INSERT INTO Sales (id, productId, soldTimestamp, transactionType, storeId) VALUES (%s, %s, %s, %s, %s)", (o[2], o[3], o[4], o[5], storeId))
+                if o[5] == 'RETURN':
+                    cursor.execute("DELETE FROM Sales WHERE id='" + str(o[2]) + "' ")
+                    deleted = deleted + 1
+                else:
+                    cursor.execute("INSERT INTO Sales (id, productId, soldTimestamp, transactionType, storeId) VALUES (%s, %s, %s, %s, %s)", (o[2], o[3], o[4], o[5], storeId))
+                    added = added + 1
                 cnxn.commit()
                 added = added + 1
             except Exception as e:
                 print(str(e))
-                deleted = deleted + 1
-            print("Added: " + str(added) + ", Errors: " + str(deleted))
+                errpr = error + 1
+            print("Added: " + str(added) + ", Deleted: " + str(deleted) + ", Errors: " + str(error))
             
