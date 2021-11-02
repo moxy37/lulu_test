dd495616b29dfb1b9349851c934c540f9e482997
moxy37 <dbrennan@nu-worx.com>
2021-11-02 00:08:20 -0600

    Changes to the solution

--- lulu_test/data/views.sql
+++ lulu_test/data/views.sql
@@ -1,18 +1,26 @@
+CREATE INDEX EpcMovement_id_productId_storeId ON EpcMovement (id, productId, storeId);
+
 DROP VIEW IF EXISTS LastRead;
 CREATE VIEW LastRead AS SELECT id, MAX(idx) AS idx FROM EpcMovement GROUP BY id ;
+
 DROP VIEW IF EXISTS CurrentLocation;
-CREATE VIEW CurrentLocation AS SELECT cl.id, m.productId, m.deptCode, m.deptName, m.subDeptCode, m.subDeptName, m.classCode, m.className, m.subClassCode, m.subClassName, m.styleCode, m.styleName, m.price, m.x, m.y, m.z, m.confidence, m.ts, m.yyyy, m.mm, m.dd, m.isDeleted, m.isDeparture, m.isExit, m.isGhost, m.isMissing, m.isMove, m.isReacquired, m.isRegion, m.isSold, m.isValid FROM LastRead cl JOIN EpcMovement m ON cl.idx=m.idx;
+CREATE VIEW CurrentLocation AS SELECT cl.id, m.productId, p.deptCode, p.deptName, p.subDeptCode, p.subDeptName, p.classCode, p.className, p.subClassCode, p.subClassName, p.styleCode, p.styleName, p.price, m.x, m.y, m.z, m.confidence, m.ts, m.yyyy, m.mm, m.dd, m.isDeleted, m.isDeparture, m.isExit, m.isGhost, m.isMissing, m.isMove, m.isReacquired, m.isRegion, m.isSold, m.isValid FROM LastRead cl JOIN EpcMovement m ON cl.idx=m.idx JOIN Products p ON m.productId=p.sku;
 
 DROP VIEW IF EXISTS AllDept;
+CREATE VIEW AllDept AS SELECT p.deptCode, p.deptName, COUNT(*) AS total, x.storeId  FROM (SELECT storeId, id, productId FROM EpcMovement GROUP BY storeId, productId, id)  AS x JOIN Products p ON x.productId=p.sku GROUP BY x.storeId, p.deptCode, p.deptName;
+
 DROP VIEW IF EXISTS AllSubDept;
+CREATE VIEW AllSubDept AS SELECT p.deptCode, p.deptName, p.subDeptCode, p.subDeptName, COUNT(*) AS total, x.storeId  FROM (SELECT storeId, id, productId FROM EpcMovement GROUP BY storeId, productId, id)  AS x JOIN Products p ON x.productId=p.sku GROUP BY x.storeId, p.deptCode, p.deptName, p.subDeptCode, p.subDeptName;
+
 DROP VIEW IF EXISTS AllClass;
+CREATE VIEW AllClass AS SELECT p.deptCode, p.deptName, p.subDeptCode, p.subDeptName, p.classCode, p.className, COUNT(*) AS total, x.storeId  FROM (SELECT storeId, id, productId FROM EpcMovement GROUP BY storeId, productId, id)  AS x JOIN Products p ON x.productId=p.sku GROUP BY x.storeId, p.deptCode, p.deptName, p.subDeptCode, p.subDeptName, p.classCode, p.className;
+
 DROP VIEW IF EXISTS AllSubClass;
+CREATE VIEW AllSubClass AS SELECT p.deptCode, p.deptName, p.subDeptCode, p.subDeptName, p.classCode, p.className, p.subClassCode, p.subClassName, COUNT(*) AS total, x.storeId  FROM (SELECT storeId, id, productId FROM EpcMovement GROUP BY storeId, productId, id)  AS x JOIN Products p ON x.productId=p.sku GROUP BY x.storeId, p.deptCode, p.deptName, p.subDeptCode, p.subDeptName, p.classCode, p.className, p.subClassCode, p.subClassName;
+
 DROP VIEW IF EXISTS AllStyle;
+CREATE VIEW AllStyle AS SELECT p.deptCode, p.deptName, p.subDeptCode, p.subDeptName, p.classCode, p.className, p.subClassCode, p.subClassName, p.styleCode, p.styleName, COUNT(*) AS total, x.storeId  FROM (SELECT storeId, id, productId FROM EpcMovement GROUP BY storeId, productId, id)  AS x JOIN Products p ON x.productId=p.sku GROUP BY x.storeId, p.deptCode, p.deptName, p.subDeptCode, p.subDeptName, p.classCode, p.className, p.subClassCode, p.subClassName, p.styleCode, p.styleName;
 
-CREATE VIEW AllDept AS SELECT deptCode, deptName, COUNT(*) AS total FROM CurrentLocation WHERE deptCode IS NOT NULL GROUP BY deptCode, deptName;
-CREATE VIEW AllSubDept AS SELECT deptCode, deptName, subDeptCode, subDeptName, COUNT(*) AS total FROM CurrentLocation WHERE deptCode IS NOT NULL GROUP BY deptCode, deptName, subDeptCode, subDeptName;
-CREATE VIEW AllClass AS SELECT deptCode, deptName, subDeptCode, subDeptName, classCode, className, COUNT(*) AS total FROM CurrentLocation WHERE deptCode IS NOT NULL GROUP BY deptCode, deptName, subDeptCode, subDeptName, classCode, className;
-CREATE VIEW AllSubClass AS SELECT deptCode, deptName, subDeptCode, subDeptName, classCode, className, subClassCode, subClassName, COUNT(*) AS total FROM CurrentLocation WHERE deptCode IS NOT NULL GROUP BY deptCode, deptName, subDeptCode, subDeptName, classCode, className, subClassCode, subClassName;
-CREATE VIEW AllStyle AS SELECT deptCode, deptName, subDeptCode, subDeptName, classCode, className, subClassCode, subClassName, styleCode, styleName, COUNT(*) AS total FROM CurrentLocation WHERE deptCode IS NOT NULL GROUP BY deptCode, deptName, subDeptCode, subDeptName, classCode, className, subClassCode, subClassName, styleCode, styleName;
-
+DROP VIEW IF EXISTS EpcMovementView;
+CREATE VIEW EpcMovementView AS SELECT m.storeId, m.regionId, m.id, m.idx, m.productId, p.deptCode, p.deptName, p.subDeptCode, p.subDeptName, p.classCode, p.className, p.subClassCode, p.subClassName, p.styleCode, p.styleName, p.price, m.x, m.y, m.z, m.confidence, m.ts, m.yyyy, m.mm, m.dd, m.isDeleted, m.isDeparture, m.isExit, m.isGhost, m.isMissing, m.isMove, m.isReacquired, m.isRegion, m.isSold, m.isValid, r.regionName, s.storeName FROM EpcMovement m JOIN Products p ON m.productId=p.sku JOIN Regions r ON m.regionId=r.id JOIN Stores s ON m.storeId=s.id;
 
