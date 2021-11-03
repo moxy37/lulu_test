b0b11dbe0a6b3eab703f8c13c9ec5ec9e2080f85
moxy37 <dbrennan@nu-worx.com>
2021-11-02 23:59:37 -0600

    as

--- dude.py
+++ dude.py
@@ -2,6 +2,7 @@ from datetime import datetime,timezone
 import requests
 from dateutil.relativedelta import relativedelta
 from time import sleep
+import math
 import mysql.connector
 cnxn = mysql.connector.connect(host="localhost", user="luluuser", passwd="Moxy..37Moxy..37", database="lulu")
 cccc = mysql.connector.connect(host="localhost", user="luluuser", passwd="Moxy..37Moxy..37", database="lulu")
@@ -11,12 +12,15 @@ added = 0
 errors = 0
 
 try:
+    print("Loading")
     d3 = dddd.cursor()
     d3.execute("SELECT id, productId, storeId, regionId, ts, x, y, confidence FROM EpcMovement ORDER BY id, ts")
     lastX = 0
     lastY = 0
     lastId = ''
+    print("Query done fetching")
     myresult = d3.fetchall()
+    print("Got em")
     for r in myresult:
         storeId = r[2]
         productId = o[1]
@@ -25,10 +29,11 @@ try:
         ts = o[4]
         x = o[5]
         y = o[6]
+        dLast = math.sqrt((x - lastX)*(x - lastX) + (y - lastY)*(y - lastY))
         confidence = o[7]
         try:
             cn = cnxn.cursor()
-            cn.execute("INSERT INTO EpcMoveCombined (id, productId, storeId, regionId, ts, x, y, confidence, lastX, lastY) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (i, productId, storeId, regionId, ts, x, y, confidence, lastX, lastY))
+            cn.execute("INSERT INTO EpcMoveCombined (id, productId, storeId, regionId, ts, x, y, confidence, lastX, lastY, dLast) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (i, productId, storeId, regionId, ts, x, y, confidence, lastX, lastY, dLast))
             cnxn.commit()
             added = added + 1
             if lastId == i:
