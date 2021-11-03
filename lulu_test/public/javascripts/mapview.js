fd547ccb0a00c108b7dbe9098b6920c4dac1f439
moxy37 <dbrennan@nu-worx.com>
2021-11-03 08:03:52 -0600

    wewew

--- lulu_test/public/javascripts/mapview.js
+++ lulu_test/public/javascripts/mapview.js
@@ -269,6 +269,9 @@ function Forward() {
         var h2 = '<h4>' + o.name + ' - ' + o.timestamp + ' Step ' + t + ' of ' + list.length + ' in ' + o.regionName + ' IsExit: ' + o.isExit + '. IsDeparture: ' + o.isDeparture + '</h4>';
         $("#Message").append(h2);
         $("#Message").show();
+        $("#menu").empty();
+        $("#menu").append(h2);
+        
     } else {
 
         $(".pointContainer li").removeClass('newpoint');
@@ -361,6 +364,8 @@ function ShowAll() {
     var h2 = '<h4>Centroid X: ' + gX + ', Y: ' + gY + '. SD: ' + gSD + '</h4>';
     $("#Message").empty();
     $("#Message").append(h2);
+    $("#menu").empty();
+    $("#menu").append(h2);
     var xx = Math.floor(gPosX + (gX - gSD) * width / imageWidth - 8);
     var yy = Math.floor(gPosY + height - (gY + gSD) * height / imageHeight - 8);
     var temp = Math.floor(gSD * 2 + 1);
