ae1a5cd9e8735177e211922249797c7e063a7a68
moxy37 <dbrennan@nu-worx.com>
2021-11-08 13:27:10 -0700

    sdsd

--- update.py
+++ update.py
@@ -74,7 +74,7 @@ while True:
                     if 'VALID' in o['events']:
                         isValid = 1
                     sql = "INSERT INTO EpcMovement (id, productId, storeId, storeName, regionId, regionName, ts, x, y, z, confidence, isDeparture, isExit, isGhost, isMissing, isMove, isReacquired, isRegion, isValid, yyyy, mm, dd) VALUES ('" + o['id'] + "', '" + o['productId'] + "', '" + o['site'] + "', '" + o['siteName'] + "', '" + o['region'] + "', '" + o['regionName'] + "', '" + str(ts) + "', " + str(o['x']) + ", " + str(o['y']) + ", " + str(o['z']) + ", " + str(o['confidence']) + ", " + str(isDeparture) + ", " + str(isExit) + ", " + str(isGhost) + ", " + str(isMissing) + ", " + str(isMove) + ", " + str(isReacquired) + ", " + str(isRegion) + ", " + str(isValid) + ", " + str(yyyy) + ", " + str(mm) + ", " + str(dd) + ")"
-                   now = ts
+                    now = ts
                     try:
                         cursor = cnxn.cursor()
                         cursor.execute(sql)
