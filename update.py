8dda8e0e5cd070bfd89324c68d5dc5a706b3a63d
moxy37 <dbrennan@nu-worx.com>
2021-12-06 13:52:08 -0700

    sd

--- update.py
+++ update.py
@@ -24,7 +24,7 @@ while True:
             deleted = 0
             updated = 0
             print("New query for store: " + str(siteId))
-            url = 'http://44.192.77.149/v1/bar/' + siteId + '/data/event/item?q=timestamp%3Agt(' + requests.utils.quote(str(now.replace(tzinfo=timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ'))) + ')&order=timestamp%3Aasc&size=2000&returnCount=false'
+            url = 'http://44.192.77.149/v1/bar/' + siteId + '/data/event/item?q=timestamp%3Agt(' + requests.utils.quote(str(now.replace(tzinfo=timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ'))) + ')&order=timestamp%3Aasc&size=20000&returnCount=false'
             #url = 'http://44.192.77.149/v1/bar/' + siteId + '/data/event/item?order=timestamp%3Aasc&size=10000&returnCount=false'
             headers = {'Authorization': 'APIKEY 2993A070-1E86-4967-8C93-D592602EDD30', 'Accept': 'application/json' , 'Content-Type': 'application/json'}
             response = requests.request("GET", url, headers=headers)
