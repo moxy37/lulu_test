8d18e1766f4d6063f3a51219b83b2870de109240
moxy37 <dbrennan@nu-worx.com>
2021-12-07 21:04:38 -0700

    Pushing test

--- lulu_test/app.js
+++ lulu_test/app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 
 var cons = require('consolidate');
 var app = express();
+const https = require('https');
 
 app.engine('html', cons.swig);
 app.set('views', path.join(__dirname, 'views'));
@@ -16,11 +17,10 @@ app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-app.listen(80, function () {
-    console.log('Spartacus Node listening on port 80!');
-});
-
+https.createServer(
+    { key: fs.readFileSync("server.key"), cert: fs.readFileSync("server.cert"), }, app).listen(3000, function () {
+        console.log("Example app listening on port 3000! Go to https://localhost:3000/");
+    });
 process.on('uncaughtException', function (err) { console.log(err); });
 //do something when app is closing
 process.on('exit', function () {
